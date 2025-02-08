64fa3613d6b86b152edbccf8fff450503c2ca909
jaejin <jaejin960729@gmail.com>
2025-02-08 12:56:48 +0000

    Feat#25 - 필요없는 파일 삭제제

--- audiolm-evaluator/evaluate_salmonn.py
+++ audiolm-evaluator/evaluate_salmonn.py
@@ -23,7 +23,7 @@ def parse_args():
         "--cfg-path", 
         type=str, 
         help='path to configuration file', 
-        default='salmonn_eval_config.yaml'
+        default='salmonn_eval_config_asr.yaml'
     )
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument(
